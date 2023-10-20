d87b914e0b5034d17b16edfb7ec9fa21647c1e84
Jackson Almeida <jackson.almeida@dismuntel.local>
2023-10-20 12:38:01 +0200

    Ordering and creating doc space

--- js/9.js
+++ js/9.js
@@ -1,3 +1,36 @@
 /* 
 Write a JavaScript function to capitalize the first letter of each word in a string.
  */
+
+
+function wordCapitalizer(text) {
+    
+
+    let p = text.split('')
+
+    /* console.log(text.split('')); */
+    console.log(p);
+
+    console.log(p.indexOf(' '));
+    
+
+    /* for (let i = 0; i < p.length; i++) {
+        
+        if (p[i] == ' ') {
+            console.log(p.indexOf(' '));
+        }
+
+        
+        
+
+    } */
+
+    for (let item of p) {
+        if (item == ' ') {
+           console.log(item);
+        }
+    
+    }
+}
+
+wordCapitalizer('deberia ir al mercado')
