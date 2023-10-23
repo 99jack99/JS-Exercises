1820d1849291f2e179a4638503f7dc50de4660f6
Jackson Almeida <jackson.almeida@dismuntel.local>
2023-10-23 09:27:55 +0200

    ex 14 done

--- js/14.js
+++ js/14.js
@@ -1,2 +1,13 @@
-/* Create a function to calculate the distance between two points defined by their x, y
-coordinates */
+/* 
+Create a function to calculate the distance between 
+two points defined by their x, y coordinates 
+*/
+
+let differenceBetween2points = (p1, p2) =>{
+
+let distance = Math.sqrt((Math.pow(p1[0] - p2[0],2) + Math.pow(p1[1] - p2[1],2))).toFixed(2)
+
+console.log(distance);
+}
+
+differenceBetween2points([1,2],[2,5])
