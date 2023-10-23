fdfffa90ddcd3ea310f3f333e1e38e8f7eb3bc33
Jackson Almeida <jackson.almeida@dismuntel.local>
2023-10-23 15:30:14 +0200

    ex 1 part 2 done

--- js/part2/1.js
+++ js/part2/1.js
@@ -2,3 +2,33 @@
 Write a JavaScript program to compare two objects to determine 
 if the first contains equivalent property values to the second one.
  */
+
+let object1 = {
+    specie: 'Human',
+    kind: 'Mammal',
+    weight: 75,
+}
+
+let object2 = {
+    specie: 'Lion',
+    kind: 'Mammal',
+    weight: 315,
+}
+
+
+let propertyComparisson = (a, b) => {
+
+    console.log(a,b);
+
+    for (let i in a) {
+       
+       if (a[i] === b[i]) {
+        console.log('Property: '+i+', value: '+a[i]);
+       }
+
+    }
+
+}
+
+propertyComparisson(object1, object2)
+
